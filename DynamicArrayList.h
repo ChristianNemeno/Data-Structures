0ad313a042d514b5557f2c188e4bd21746330269
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2024-08-24 13:35:25 +0800

    Deleted all of the previous code and changed it without any assistance.

--- DynamicArrayList.h
+++ DynamicArrayList.h
@@ -1,31 +1,41 @@
 #include "List.h"
 #include <iostream>
 using namespace std;
-class DynamicArrayList : public List
-{
-private:
-    int array[5];
-    int size=0;
-    
 
-    /* data */
+class DynamicArrayList : public List {
+private:
+    int* array;
+    int size;
+    int capacity;
+
+    void resize(int newCapacity){
+        cout << "Resizing..." << endl;
+        int* newArray = new int[newCapacity];
+        for(int i=0; i<size; i++){
+            newArray[i] = array[i];
+        }
+        delete[] array;
+        array = newArray;
+        capacity = newCapacity;
+    }
+
+
 public:
-    void add(int n){
-        if(size == 4){
-            cout << "Its full na";
-            return;
+    DynamicArrayList(int theCapacity) : size(0), capacity(theCapacity) {
+        array = new int[capacity];
+    }
+
+//add 20 jump rope
+     void add(int n){
+        if(size == capacity){
+            resize(capacity * 2);
         }
         array[size++] = n;
-        
-    }
-    int remove(int n){
-        if(size == 0){
-            return -1;
-        }
-
-        for(int i=0; i<size; i++){
+     }
+     int remove(int n){
+        for(int i=0;i<size-1;i++){
             if(array[i] == n){
-                for(int j=i; j<size-1;j++){
+                for(int j=i; j<size-1; j++){
                     array[i] = array[i+1];
                 }
                 size--;
@@ -33,15 +43,11 @@ public:
             }
         }
         return -1;
-    }
-
-    void print(){
-        for(int i=0 ; i<size; i++){
+     }
+     void print(){
+        for(int i=0; i<size;i++){
             cout << array[i] << " ";
         }
-    }
-
+        cout << endl;
+     }
 };
-
-
-
--- main.cpp
+++ main.cpp
@@ -4,13 +4,31 @@
 
 int main(){
 
-    List* list = new DynamicArrayList();
+    List* list = new DynamicArrayList(5);
 
-    list->add(13);
-    list->add(11);
-    list->add(8);
-    int pos = list -> remove(11);
+    list->add(1);
+    list->add(2);
+    list->add(3);
+    int pos = list -> remove(2);
     cout << "position: "<< pos << " " << endl;
+    list->print();
+
+    list->add(4);
+    list->print();
+    
+    list->add(5);
+    list->print();
+    
+    list->add(6);
+    list->print();
+    
+    list->add(7);
+    list->print();
+
+    list->add(8);
+    list->print();
+    
+    list->add(9);
     list->print();
 
 
