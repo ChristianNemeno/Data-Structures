f651615c15dce48c003c3e47a0b81d92affcbcd3
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2024-09-07 11:44:33 +0800

    yay

--- Course/main.cpp
+++ Course/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	cout << sizeof(Course);
+	
 	Course* csit = new Course();
 	student* s;
 	char op;
